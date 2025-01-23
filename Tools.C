355509c0faa08e607805efd06b6feb3261d16dc7
Narwar04 <carenathan027@gmail.com>
2025-01-23 16:52:47 -0500

    added the new main.c and passed the new test

--- Tools.C
+++ Tools.C
@@ -250,9 +250,11 @@ uint64_t Tools::copyBits(uint64_t source, uint64_t dest,
  */
 uint64_t Tools::setByte(uint64_t source, int32_t byteNum)
 {
-  if(byteNum < 0 || byteNum > 7) return source;
+    if (byteNum < 0 || byteNum >= 8) return source;
 
-  return source | (0xff << (byteNum * 8)) ; 
+    uint64_t mask = 0xFFULL << (byteNum * 8);
+
+    return source | mask; 
 }
 
 
